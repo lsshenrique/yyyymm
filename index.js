0800307fb80b90baf6056ee4d29641adb6e11a3c
Henrique L S Silva <lsshenrique@hotmail.com>
2021-02-25 21:11:06 -0300

    git init

---
+++ index.js
@@ -0,0 +1,29 @@
+var moment = require('moment')
+
+const FORMAT = "YYYYMM"
+
+module.exports = {
+    addMonth(value, amount) {
+        return parseInt(moment(value, FORMAT).add(amount, 'month').format(FORMAT))
+    },
+    asMoment(value, setDate) {
+        let aux = moment(value, FORMAT)
+
+        if (setDate) {
+            aux.set('date', setDate)
+        }
+
+        return aux
+    },
+    rangeByIncr(value, increment) {
+        const values = [value]
+        const base = moment(value, FORMAT)
+
+        for (let amount = 1; amount <= increment; amount++) {
+            const amAux = parseInt(base.add(1, 'month').format(FORMAT))
+            values.push(amAux)
+        }
+
+        return values
+    }
+}
